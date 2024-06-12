bdbbeeeee5f900796057d3155bcd701bb9560867
GavrilS <gavr1lll@abv.bg>
2024-06-12 07:49:32 +0300

    Updated the docs with the process steps and filled the dissect and check_for_exit functionality of the program

--- random_number_generator/doc.md
+++ random_number_generator/doc.md
@@ -1,1 +1,7 @@
 # Console application to generate random numbers until the user wants them.
+
+* Process:
+ - show the user a manual of how to use the console application when the program runs initially
+ - ask the user for input on the range and number of random numbers needed
+ - give the user a chance to exit (might merge this with the above step)
+ - dissect the user input and extract the relevant values or inform the user for wrong input and give instructions again
--- random_number_generator/generate_numbers.py
+++ random_number_generator/generate_numbers.py
@@ -1,20 +1,52 @@
 from random import randrange
 
 START_RANGE = 0
+END_RANGE = 10
+NUM_COUNT = 1
 
 
 def main():
     flag = True
     while flag:
         details = input("What's the range you want to use:")
-        details_array = details.split(',')
-        range_details = details_array[0].strip()
-        if '-' in range_details:
-            range_details = range_details.split('-')
-            START_RANGE = range_details[0]
-            end_range = range_details[1]
-        else:
-            end_range = range_details
+
+
+def show_instructions():
+    pass
+
+
+def dissect_input(user_input):
+    details_array = user_input.split(',')
+    range_details = details_array[0].strip()
+    if '-' in range_details:
+        range_details = range_details.split('-')
+        START_RANGE = int(range_details[0])
+        END_RANGE = int(range_details[1])
+    else:
+        END_RANGE = int(range_details)
+    
+    if len(details_array) > 1:
+        NUM_COUNT = int(details_array[1])
+    
+    return START_RANGE, END_RANGE, NUM_COUNT
+
+
+def check_for_exit(user_input):
+    if 'exit' in user_input.lower():
+        print('Thank you for using the application!')
+        return False
+    else:
+        return True
+
+
+def verify_input(user_input):
+    pass
+
+
+def reset_defaults():
+    START_RANGE = 0
+    END_RANGE = 10
+    NUM_COUNT = 1
 
 
 
