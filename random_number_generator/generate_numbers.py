aacc1a6b8c03aacc1be51698b587249056bd375e
GavrilS <gavr1lll@abv.bg>
2024-06-14 06:28:28 +0300

    Added instructions and error handling

--- random_number_generator/generate_numbers.py
+++ random_number_generator/generate_numbers.py
@@ -6,6 +6,7 @@ NUM_COUNT = 1
 
 
 def main():
+    show_instructions()
     flag = True
     while flag:
         details = input("What's the range you want to use:")
@@ -17,6 +18,7 @@ def main():
 
         if value_error:
             print(value_error)
+            show_instructions()
             continue
 
         get_numbers(start, end, num_count)
@@ -25,7 +27,18 @@ def main():
 
 
 def show_instructions():
-    pass
+    instructions = "This program is generating random numbers in the user specified range. \n" \
+        "If the user does not provide a range when prompted, the defaults will be used.\n" \
+        "Defaults: Start range number - 0; End range number - 10; Number of randomly generated numbers - 1.\n" \
+        "The format of the range you can specify is START_NUMBER-END_NUMBER,NUMBER_COUNT_TO_GENERATE.\n" \
+        "Example: What's the range you want to use: 10-100,5 will generate 5 random numbers between 10 and 100.\n" \
+        "You can skip the start number and the number count parameters and the default values will be used.\n" \
+        "Type 'exit' to stop the program."
+
+    print('-------------------')
+    print('Instructions:')
+    print(instructions)
+    print('-------------------')
 
 
 def get_numbers(start, end, num_count):
@@ -39,23 +52,26 @@ def get_numbers(start, end, num_count):
 def dissect_input(user_input):
     global START_RANGE, END_RANGE, NUM_COUNT
     value_error = None
-    print(f"Start - {START_RANGE}, End - {END_RANGE}, Count - {NUM_COUNT} in dissect input")
-    details_array = user_input.split(',')
-    range_details = details_array[0].strip()
-    if '-' in range_details:
-        range_details = range_details.split('-')
-        START_RANGE = int(range_details[0])
-        END_RANGE = int(range_details[1])
-    else:
-        END_RANGE = int(range_details)
-    
-    if len(details_array) > 1:
-        NUM_COUNT = int(details_array[1])
+    try:
+        print(f"Start - {START_RANGE}, End - {END_RANGE}, Count - {NUM_COUNT} in dissect input")
+        details_array = user_input.split(',')
+        range_details = details_array[0].strip()
+        if '-' in range_details:
+            range_details = range_details.split('-')
+            START_RANGE = int(range_details[0])
+            END_RANGE = int(range_details[1])
+        else:
+            END_RANGE = int(range_details)
+        
+        if len(details_array) > 1:
+            NUM_COUNT = int(details_array[1])
 
-    if START_RANGE > END_RANGE:
-        value_error = 'The start of the random range cannot be bigger than the end! Please put correct ranges!'
-    
-    return START_RANGE, END_RANGE, NUM_COUNT, value_error
+        if START_RANGE > END_RANGE:
+            value_error = 'The start of the random range cannot be bigger than the end! Please put correct ranges!'
+        
+        return START_RANGE, END_RANGE, NUM_COUNT, value_error
+    except Exception as e:
+        return START_RANGE, END_RANGE, NUM_COUNT, str(e)
 
 
 def check_for_exit(user_input):
